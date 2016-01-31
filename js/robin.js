88d4789a8183cca99c4611c9864a73d6493266d1
Lisa Xu <lisa.yaqing.xu@gmail.com>
2016-01-31 05:29:01 -0500

    fixed positioning

--- js/robin.js
+++ js/robin.js
@@ -371,12 +371,12 @@ function hairfill(img,color){
 	ttx.putImageData(imgdata,0,0);
 }
 
-function random(){
+function randomize(){
 	currentrobin = Math.floor(Math.random()*6);
 	currentHair = Math.floor(Math.random()*5);
 	currentFace = Math.floor(Math.random()*5);
 	currentColor = Math.floor(Math.random()*colorList.length);
-	changeAllMenu();
+	changeAllMenu();	
 	loadCurrentrobin();	
 }
 
